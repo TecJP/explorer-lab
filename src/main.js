31d23256373aaf8d43931119572eb870bd2102d6
TecJP <tec.joao.freitas@gmail.com>
2022-10-25 14:38:10 -0300

    Added the array of cards data, create the function to push the data to the array

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import "./css/index.css";
 import IMask from "imask";
 
 /**
+ * Section of card data
+ */
+const cardData = [];
+console.log(cardData);
+
+/**
  * Section of colors
  */
 const ccBgColor01 = document.querySelector(
@@ -25,7 +31,7 @@ function setCardType(type) {
   ccBgColor02.setAttribute("fill", colors[type][1]);
   ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
-setCardType("maestro");
+setCardType("default");
 
 /**
  * Section of security code
@@ -88,7 +94,7 @@ const cardNumberPattern = {
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: /^((5(([1-2]|[4-5])[0-9]{8}|0((1|6)([0-9]{7}))|3(0(4((0|[2-9])[0-9]{5})|([0-3]|[5-9])[0-9]{6})|[1-9][0-9]{7})))|((508116)\\d{4,10})|((502121)\\d{4,10})|((589916)\\d{4,10})|(2[0-9]{15})|(67[0-9]{14})|(506387)\\d{4,10})/,
+      regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
       cardType: "mastercard",
     },
     {
@@ -98,7 +104,8 @@ const cardNumberPattern = {
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+      regex:
+        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
       cardType: "elo",
     },
     {
@@ -136,10 +143,30 @@ addButton.addEventListener("click", () => {
 });
 
 /**
+ * Clear the inputs
+ */
+function clearInputs() {}
+
+/**
+ * Push data to array
+ */
+function sendDataToCardData() {
+  cardData.push({
+    cardNumber: cardNumberMasked.value,
+    cardHolder: cardHolder.value.toUpperCase(),
+    expirationDate: expirationDateMasked.value,
+    verificationCode: securityCodeMasked.value,
+    cardType: cardNumberMasked.masked.currentMask.cardType,
+  });
+}
+
+/**
  * Submit Section
  */
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
+  sendDataToCardData();
+  clearInputs();
 });
 
 /**
